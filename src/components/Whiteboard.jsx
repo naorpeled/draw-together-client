ac5980146bcecbf48b64a345570d05bd758ae5d1
naorpeled <thehecticbyte@gmail.com>
2020-02-20 22:31:02 +0200

    Added basic functionality to the canvas - Now you can draw and see the changes on the canvas - Now you can see others drawing - Now you can clear the canvas

--- src/components/Whiteboard.jsx
+++ src/components/Whiteboard.jsx
@@ -8,6 +8,8 @@ export default class GameScreen extends Component {
     constructor(props) {
         super(props);
 
+        this.canvas = React.createRef();
+
         socket.on('connect', function(){
             console.log('Connected');
         });
@@ -16,13 +18,52 @@ export default class GameScreen extends Component {
         });
     }
 
+    componentDidMount() {
+        socket.on('onDraw', (data) => {
+            const context = this.canvas.current.getContext("2d")
+            context.beginPath();
+            context.arc(data.x, data.y, 7.5, 0, Math.PI * 2, false);
+            context.fill();
+            context.lineWidth = 5;
+            context.strokeStyle = "#00000";
+            context.stroke();
+        
+
+        });
+        socket.on('onCanvasClear', (data) => {
+            const ref = this.canvas.current;
+            ref.getContext("2d").clearRect(0, 0, ref.width, ref.height);
+        });
+    }
+
+    draw = (e) => {
+        socket.emit('onDraw', {
+            x: e.clientX,
+            y: e.clientY
+        });
+
+        const context = this.canvas.current.getContext("2d")
+        context.beginPath();
+        context.arc(e.clientX, e.clientY, 7.5, 0, Math.PI * 2, false);
+        context.fill();
+        context.lineWidth = 5;
+        context.strokeStyle = "#00000";
+        context.stroke();
+    }
+
+    clearCanvas = () => {
+        socket.emit('onCanvasClear');
+        const ref = this.canvas.current;
+        ref.getContext("2d").clearRect(0, 0, ref.width, ref.height);
+    }
     render() {
         return (
             <div className="game">
                 <AppContext.Consumer>
                     {(context) => (
                        <Fragment>
-                            
+                            <canvas onMouseMove={(e) => this.draw(e)} ref={this.canvas}></canvas>
+                            <button onClick={() => this.clearCanvas()}>Clear</button>
                        </Fragment>
                     )}
                 </AppContext.Consumer>
@@ -30,5 +71,3 @@ export default class GameScreen extends Component {
         )
     }
 }
-
-
