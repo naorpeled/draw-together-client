054b66a7a3e9cd00e11f52b4b796e24ad42285ff
naorpeled <thehecticbyte@gmail.com>
2020-02-23 15:49:08 +0200

    Added links to the main page

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,5 +1,7 @@
 import React, { Component, Fragment } from 'react'
 import AppContext from '../context/AppContext'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 export default class Welcome extends Component {
 
@@ -13,7 +15,7 @@ export default class Welcome extends Component {
 
     render() {
         return (
-            <div className="login">
+            <div id="login">
                 <div className="login-container">
                     <img className="app-title logo" src={require('../assets/logo.webp')} alt="Logo" />
                     <AppContext.Consumer>
@@ -43,6 +45,20 @@ export default class Welcome extends Component {
                     </AppContext.Consumer>
                 </div>
                 {this.state.error && <span className="error">{this.state.error}</span>}
+                <hr />
+                <a href="https://github.com/naorpeled" className="link left">
+                    <FontAwesomeIcon
+                        icon={faGithub}
+                        onClick={() => console.log('test')}
+                        ></FontAwesomeIcon> Github
+                </a>
+                <a href="https://www.linkedin.com/in/naorpeled/" className="link right">
+                    <FontAwesomeIcon
+                        color="#2867B2"
+                        icon={faLinkedin}
+                        onClick={() => console.log('test')}
+                        ></FontAwesomeIcon> LinkedIn
+                </a>
             </div>
         )
     }
